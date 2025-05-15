2f7cd80162b2c1b436b2c885454cd87ddcffe409
ferhat-hachemi <hachemi.ferhat25@gmail.com>
2025-05-15 15:17:17 +0200

    🚀 initial project setup

---
+++ src/main/java/dev/hachemi/mystipass/MystipassApplication.java
@@ -0,0 +1,13 @@
+package dev.hachemi.mystipass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystipassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystipassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/hachemi/mystipass/MystipassApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hachemi.mystipass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystipassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
