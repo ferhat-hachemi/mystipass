4839548d4c48acd7a6d442402fe5d443c9a8d80b
ferhat-hachemi <hachemi.ferhat25@gmail.com>
2025-05-21 11:14:50 +0200

    📝 enhance Javadoc for interface methods to improve clarity and maintainability

--- src/main/java/dev/hachemi/mystipass/service/MystipassService.java
+++ src/main/java/dev/hachemi/mystipass/service/MystipassService.java
@@ -1,29 +1,36 @@
 package dev.hachemi.mystipass.service;
 
+/**
+ * Interface for managing mystipass cli operations.
+ */
 public interface MystipassService {
+
 
     /**
      * Initializes the credential storage or any required setup.
+     * @return A message indicating the result of the initialization.
      */
     String init();
 
     /**
      * Adds a new credential entry with the given key, username, and password.
-     * @param key
-     * @param username
-     * @param password
+     * @param key A unique key to identify this password entry
+     * @param username The username or email linked to this entry
+     * @param password The password you want to securely store
+     * @return A message indicating the result of the operation.
      */
     String add(String key, String username, String password);
 
     /**
      * Lists all stored entries
+     * @return A formatted string containing all stored entries.
      */
     String list();
 
     /**
-     * Retreive entires with the given key
-     * @param key
-     * @return
+     * Retrieve entry with the given key
+     * @param key The key used to identify the password entry
+     * @return The entry information as a formatted string, or an error message if not found.
      */
     String get(String key);
 }
